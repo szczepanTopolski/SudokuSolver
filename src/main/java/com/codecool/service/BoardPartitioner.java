472cf35913eaa8a54ac40d2e6b7bdb83168d946f
PatrykTopolski <venirro@gmail.com>
2020-08-12 14:58:35 +0200

    refactored minor value mistake in board partitioner

--- src/main/java/com/codecool/service/BoardPartitioner.java
+++ src/main/java/com/codecool/service/BoardPartitioner.java
@@ -5,17 +5,18 @@ import com.codecool.model.Cell;
 
 
 public  class BoardPartitioner {
-    private static final int width = 9;
+    private static final int WIDTH = 9;
+    private static final int HEIGHT = 9;
 
 
 
     public static Cell getCell(Board board, int x, int y) {
-        return board.getCells()[y * width + x];
+        return board.getCells()[y * WIDTH + x];
     }
 
     public static Cell[] getColumn(Board board, int index){
         Cell[] column = new Cell[9];
-        for (int i = 0; i <board.getCells().length ; i++) {
+        for (int i = 0; i <HEIGHT; i++) {
             column[i] = getCell(board, index, i);;
         }
         return column;
