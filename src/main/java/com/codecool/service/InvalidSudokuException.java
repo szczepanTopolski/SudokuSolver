e8de83db673d4b5bd294d4bada974f9a78322252
PatrykTopolski <venirro@gmail.com>
2020-08-12 12:39:11 +0200

    added custom sudoku exception

---
+++ src/main/java/com/codecool/service/InvalidSudokuException.java
@@ -0,0 +1,8 @@
+package main.java.com.codecool.service;
+
+public class InvalidSudokuException extends Exception {
+
+    public InvalidSudokuException(String message) {
+        super(message);
+    }
+}
