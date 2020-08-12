f1e853a887d03ad2140882460b9276b75fa06703
PatrykTopolski <venirro@gmail.com>
2020-08-12 14:10:33 +0200

    changed partitioner to static

--- src/main/java/com/codecool/service/BoardPartitioner.java
+++ src/main/java/com/codecool/service/BoardPartitioner.java
@@ -3,25 +3,20 @@ package com.codecool.service;
 import com.codecool.model.Board;
 import com.codecool.model.Cell;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
 
-public class BoardPartitioner {
-    private final int width = 9;
-    Cell[] cells;
+public  class BoardPartitioner {
+    private static final int width = 9;
 
-    public BoardPartitioner(Board board) {
-        this.cells = board.getCells();
+
+
+    public static Cell getCell(Board board, int x, int y) {
+        return board.getCells()[y * width + x];
     }
 
-    public Cell getCell(int x, int y) {
-        return cells[y * width + x];
-    }
-
-    public Cell[] getColumn(int index){
-        Cell[] column = new Cell[8];
-        for (int i = 0; i <cells.length ; i++) {
-            column[i] = getCell(index, i);;
+    public static Cell[] getColumn(Board board, int index){
+        Cell[] column = new Cell[9];
+        for (int i = 0; i <board.getCells().length ; i++) {
+            column[i] = getCell(board, index, i);;
         }
         return column;
     }
