bded1933ff881185e238378f4eb0c84bd7437f9e
PatrykTopolski <venirro@gmail.com>
2020-08-14 14:21:23 +0200

    added tests for row validation searching for duplicates

---
+++ src/test/java/com/codecool/service/CorrectnessValidatorTest.java
@@ -0,0 +1,35 @@
+package com.codecool.service;
+
+import com.codecool.exception.InvalidSudokuException;
+import com.codecool.model.Board;
+import com.codecool.model.Cell;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CorrectnessValidatorTest {
+    private Board boardStub;
+    private Cell[] cellsStub;
+    private final int DUPLICATE_TEST_VALUE = 82;
+
+    @BeforeEach
+    void init(){
+        cellsStub = new Cell[81];
+        for (int i = 0; i < 81; i++) {
+            cellsStub[i] = new Cell(i,i,i,i);
+        }
+        boardStub = new Board(cellsStub);
+    }
+
+    @Test
+    @DisplayName("Test board for duplicates")
+    public void testRowValidator(){
+        boardStub.getCells()[0].setValue(DUPLICATE_TEST_VALUE);
+        boardStub.getCells()[1].setValue(DUPLICATE_TEST_VALUE);
+        CorrectnessValidator correctnessValidator = new CorrectnessValidator(boardStub);
+        Assertions.assertThrows(InvalidSudokuException.class, correctnessValidator::validate);
+    }
+
+
+}
