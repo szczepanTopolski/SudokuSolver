0db2480db2bb7535853e50d7ef0f57a089be663b
PatrykTopolski <venirro@gmail.com>
2020-08-14 14:21:00 +0200

    added row validation searching for duplicates

---
+++ src/main/java/com/codecool/service/CorrectnessValidator.java
@@ -0,0 +1,45 @@
+package com.codecool.service;
+
+import com.codecool.exception.InvalidSudokuException;
+import com.codecool.model.Board;
+import com.codecool.model.Cell;
+
+import java.util.*;
+
+import static com.codecool.service.BoardPartitioner.*;
+
+
+public class CorrectnessValidator {
+    Board board;
+
+    public CorrectnessValidator(Board board) {
+        this.board = board;
+    }
+
+    public void validate() throws InvalidSudokuException{
+        validateBoxes();
+        validateCols();
+        validateRows();
+    }
+
+    private void validateRows() throws InvalidSudokuException {
+        for (int i = 0; i < 9; i += 9) {
+            List<Cell> cells = Arrays.asList(getRow(board, i));
+            List<Integer> cellValues = new ArrayList<>();
+            cells.forEach(cell -> cellValues.add(cell.getValue()));
+            Set<Integer> testSet = new HashSet<>(cellValues);
+            if(testSet.size() < cellValues.size()){
+                String message = "duplicates found in row" + i;
+                throw new InvalidSudokuException(message);
+            }
+        }
+
+    }
+    private void validateCols() throws InvalidSudokuException {
+
+    }
+
+    private void validateBoxes() throws InvalidSudokuException {
+
+    }
+}
