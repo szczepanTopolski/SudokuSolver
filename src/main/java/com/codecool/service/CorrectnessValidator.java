2fc50baaf0d721d5560d4d9e4fb470d0a8bb07d4
PatrykTopolski <venirro@gmail.com>
2020-08-18 15:03:55 +0200

    fix/ fixed col iteration

--- src/main/java/com/codecool/service/CorrectnessValidator.java
+++ src/main/java/com/codecool/service/CorrectnessValidator.java
@@ -23,7 +23,7 @@ public class CorrectnessValidator {
     }
 
     private void validateRows() throws InvalidSudokuException {
-        for (int i = 0; i < 9; i += 9) {
+        for (int i = 0; i < 81; i += 9) {
             List<Cell> cells = Arrays.asList(getRow(board, i));
             List<Integer> cellValues = new ArrayList<>();
             cells.forEach(cell -> cellValues.add(cell.getValue()));
@@ -42,7 +42,7 @@ public class CorrectnessValidator {
             cells.forEach(cell -> cellValues.add(cell.getValue()));
             Set<Integer> testSet = new HashSet<>(cellValues);
             if(testSet.size() < cellValues.size()){
-                String message = "duplicates found in row" + i;
+                String message = "duplicates found in column" + i;
                 throw new InvalidSudokuException(message);
             }
         }
