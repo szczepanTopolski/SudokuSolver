eda896c459874df16e433258d3afcaa4fad6f70c
szczepanTopolski <szczepanPL678@gmail.com>
2020-08-19 15:41:44 +0200

    Add getBoard with solved sudoku

--- src/test/java/com/codecool/service/TestUtills.java
+++ src/test/java/com/codecool/service/TestUtills.java
@@ -8,8 +8,8 @@ import java.util.HashSet;
 
 public class TestUtills {
 
-    public Board getBoard() {
-        int[][] values = getValues();
+    public Board getBoard(boolean solved) {
+        int[][] values = solved ? getSolvedValues() : getValues();
         Cell[] cells = new Cell[81];
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -33,6 +33,17 @@ public class TestUtills {
                             {0, 0, 5, 2, 0, 6, 3, 0, 0}};
     }
 
+private int[][] getSolvedValues() {
+    return new int[][]{ {3, 1, 6, 5, 7, 8, 4, 9, 2},
+            {5, 2, 9, 1, 3, 4, 7, 6, 8},
+            {4, 8, 7, 6, 2, 9, 5, 3, 1},
+            {2, 6, 3, 4, 1, 5, 9, 8, 7},
+            {9, 7, 4, 8, 6, 3, 1, 2, 5},
+            {8, 5, 1, 7, 9, 2, 6, 4, 3},
+            {1, 3, 8, 9, 4, 7, 2, 5, 6},
+            {6, 9, 2, 3, 5, 1, 8, 7, 4},
+            {7, 4, 5, 2, 8, 6, 3, 1, 9}};
+}
     private void fillWithPossibilitiesWhenHasNoValue(Cell cell){
         if(cell.getValue() == 0)
             cell.setPossibilities(new HashSet<>(Arrays.asList(1,2,3,4,5,6,7,8,9)));
