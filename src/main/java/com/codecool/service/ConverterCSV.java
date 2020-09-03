ecc4a0d189ce754d644d016e00dcb1fc497a18da
szczepanTopolski <szczepanPL678@gmail.com>
2020-09-03 16:03:12 +0200

    Make converter csv accept string to convert

--- src/main/java/com/codecool/service/ConverterCSV.java
+++ src/main/java/com/codecool/service/ConverterCSV.java
@@ -6,14 +6,17 @@ import com.codecool.model.Cell;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import static com.codecool.service.Properties.MAX_HEIGHT;
 import static com.codecool.service.Properties.MAX_WIDTH;
 
-public class ConverterCSV implements Converter{
+public class ConverterCSV implements Converter<Board> {
 
     @Override
-    public Board convert(String[][] values) throws IllegalArgumentException{
+    public Board convert(String input) throws IllegalArgumentException {
+        String[][] values = retrieveValuesFromString(input);
         Cell[] cells = new Cell[MAX_HEIGHT.getValue() * MAX_WIDTH.getValue()];
         for (int i = 0; i < MAX_HEIGHT.getValue(); i++) {
             for (int j = 0; j < MAX_WIDTH.getValue(); j++) {
@@ -28,9 +31,16 @@ public class ConverterCSV implements Converter{
     }
 
     private void fillPossibilitiesWhenHasNoValue(int value, Cell cell) {
-        List<Integer> possibilities = Arrays.asList(1,2,3,4,5,6,7,8,9);
-        if(value != 0) {
+        List<Integer> possibilities = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        if (value != 0) {
             cell.setPossibilities(new HashSet<>(possibilities));
         }
+    }
+
+    private String[][] retrieveValuesFromString(String string) {
+        return Arrays.stream(string.split("\n"))
+                .map(row -> Arrays.stream(row.split(";"))
+                        .toArray(String[]::new))
+                .toArray(String[][]::new);
     }
 }
