c1cd3b8bbbca61e85a09bae49c58c058fa572122
PatrykTopolski <venirro@gmail.com>
2020-09-03 15:29:18 +0200

    add/ Java fx stage with on click and dependency to convert csv to string dell/ legacy code

--- src/main/java/com/codecool/controller/RootController.java
+++ src/main/java/com/codecool/controller/RootController.java
@@ -1,110 +1,97 @@
 package com.codecool.controller;
 
+
 import com.codecool.model.Board;
-import com.codecool.model.Cell;
-import com.codecool.service.Solver;
+import com.codecool.service.Converter;
+import com.codecool.service.ConverterCSV;
+import com.codecool.service.ValidatorCSV;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.apache.commons.io.IOUtils;
+import javafx.scene.text.Text;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
-import java.util.Arrays;
-import java.util.HashSet;
 
-public class RootController {
+public class RootController extends Application {
+    ConverterCSV converterCSV = new ConverterCSV();
+    private  File file;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("multi thread Sudoku Solver");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("txt files (.txt, .csv)", "*.txt")
+        );
+        Button button = new Button("Select File");
+        ChooseFileButtonPressed(primaryStage, fileChooser, button);
+
+        VBox vBox = new VBox(button);
+        Scene initialScene = new Scene(vBox, 960, 600);
+
+        primaryStage.setScene(initialScene);
+        primaryStage.show();
+    }
+
+
+
     public static void main(String[] args) {
-        RootController root = new RootController();
-        Board board = root.getBoard(State.VALID);
-        Solver solver = new Solver(board);
-        SolverController solverController = new SolverController(solver);
-        Thread thread = new Thread(solverController);
-        thread.start();
+        Application.launch(args);
+//        RootController root = new RootController();
+//        Board board = root.getBoard(State.VALID);
+//        Solver solver = new Solver(board);
+//        SolverController solverController = new SolverController(solver);
+//        Thread thread = new Thread(solverController);
+//        thread.start();
+
+    }
+
+    private String stringFromFile(File file){
+        String contents = "";
+        try (InputStream in = new FileInputStream(file)) {
+            contents = IOUtils.toString(in, StandardCharsets.UTF_8);
+            System.out.println(contents);
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contents;
     }
 
 
-    public Board getBoard(State state) {
-        int[][] values = getValues(state);
-        Cell[] cells = new Cell[81];
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                int id = i + j * 9;
-                cells[id] = new Cell(id, i, j , values[j][i]);
-                fillWithPossibilitiesWhenHasNoValue(cells[id]);
+
+    /***
+     * Methods connected with a button onclick event handler
+     */
+    private void ChooseFileButtonPressed(Stage primaryStage, FileChooser fileChooser, Button button) {
+        button.setOnAction(e -> {
+            file = fileChooser.showOpenDialog(primaryStage);
+            ValidatorCSV vcsv = new ValidatorCSV();
+            try {
+                vcsv.validate(stringFromFile(file));
+                Text textBoard = new Text(stringFromFile(file));
+                Scene newBoardScene = new Scene(new VBox(textBoard), 960, 600);
+                primaryStage.setScene(newBoardScene);
+            }catch (Exception exception){
+                exception.printStackTrace();
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Wrong board Exception");
+                alert.setHeaderText("Something went wrong with file ");
+                alert.setContentText("check if the board is a valid board");
+                alert.showAndWait();
             }
-        }
-        return new Board(cells);
+
+        });
     }
 
-    private int[][] getValues(State state) {
-        int[][] values;
-        switch (state){
-            case VALID:
-                values = getValidValues();
-                break;
-            case SOLVED:
-                values = getSolvedValues();
-                break;
-            case INVALID:
-                values = getInvalidValues();
-                break;
-            case WORLD_HARDEST:
-                values = getWorldHardestValues();
-                break;
-            default:
-                throw new IllegalArgumentException();
-        }
-        return values;
-    }
-
-    private int[][] getInvalidValues() {
-        return new int[][]{ {3, 1, 6, 5, 7, 8, 4, 9, 2},
-                {3, 2, 9, 1, 3, 4, 7, 6, 8},
-                {4, 8, 7, 6, 2, 9, 5, 5, 1},
-                {2, 6, 4, 4, 1, 5, 9, 6, 7},
-                {9, 7, 4, 8, 6, 3, 1, 2, 5},
-                {8, 5, 5, 7, 9, 5, 6, 4, 3},
-                {1, 3, 8, 9, 4, 7, 2, 5, 6},
-                {6, 9, 2, 3, 5, 7, 8, 6, 4},
-                {7, 4, 5, 2, 8, 6, 3, 1, 9}};
-    }
-
-    private int[][] getValidValues() {
-        return new int[][]{ {0, 0, 6, 5, 0, 8, 4, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 8, 7, 0, 0, 0, 0, 3, 1},
-                {0, 0, 3, 0, 1, 0, 0, 8, 0},
-                {9, 0, 0, 8, 0, 3, 0, 0, 5},
-                {0, 5, 0, 0, 0, 0, 6, 0, 0},
-                {0, 3, 0, 0, 0, 0, 2, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 4},
-                {0, 0, 5, 2, 0, 6, 3, 0, 0}};
-    }
-
-    private int[][] getSolvedValues() {
-        return new int[][]{ {3, 1, 6, 5, 7, 8, 4, 9, 2},
-                {5, 2, 9, 1, 3, 4, 7, 6, 8},
-                {4, 8, 7, 6, 2, 9, 5, 3, 1},
-                {2, 6, 3, 4, 1, 5, 9, 8, 7},
-                {9, 7, 4, 8, 6, 3, 1, 2, 5},
-                {8, 5, 1, 7, 9, 2, 6, 4, 3},
-                {1, 3, 8, 9, 4, 7, 2, 5, 6},
-                {6, 9, 2, 3, 5, 1, 8, 7, 4},
-                {7, 4, 5, 2, 8, 6, 3, 1, 9}};
-    }
-
-    private int[][] getWorldHardestValues() {
-        return new int[][]{ {8, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 3, 6, 0, 0, 0, 0, 0},
-                {0, 7, 0, 0, 9, 0, 2, 0, 0},
-                {0, 5, 0, 0, 0, 7, 0, 0, 0},
-                {0, 0, 0, 0, 4, 5, 7, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 3, 0},
-                {0, 0, 1, 0, 0, 0, 0, 6, 8},
-                {0, 0, 8, 5, 0, 0, 0, 1, 0},
-                {0, 9, 0, 0, 0, 0, 4, 0, 0}};
-    }
-    private void fillWithPossibilitiesWhenHasNoValue(Cell cell){
-        if(cell.getValue() == 0)
-            cell.setPossibilities(new HashSet<>(Arrays.asList(1,2,3,4,5,6,7,8,9)));
-    }
-
-    enum State{
-        VALID, SOLVED, INVALID, WORLD_HARDEST
-    }
 }
