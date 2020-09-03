4b60a9e14da073775ec51e642e859da07c984c27
szczepanTopolski <szczepanPL678@gmail.com>
2020-09-03 16:04:31 +0200

    Add tests for validator

--- src/test/java/com/codecool/service/ValidatorCSVTest.java
+++ src/test/java/com/codecool/service/ValidatorCSVTest.java
@@ -13,20 +13,34 @@ class ValidatorCSVTest {
 
     @Test
     public void shouldRaiseInvalidArgumentExceptionWhenEmptyStringPassed() {
-        assertThrows(IllegalArgumentException.class, ()->validator.validate(null));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate(null));
     }
 
     @Test
     public void shouldRaiseInvalidArgumentExceptionWhenTooShortRowPassed() {
-        assertThrows(IllegalArgumentException.class, ()->validator.validate("0;1;2;3;4;5;6;7;"));
-    }
-    @Test
-    public void shouldRaiseInvalidArgumentExceptionWhenNotTooShortColsPassed() {
-        assertThrows(IllegalArgumentException.class, ()->validator.validate("0;0;0;0;0;0;0;0;0;\n0;0;0;0;0;0;0;0;0;\n"));
-    }
-    @Test
-    public void shouldRaiseInvalidArgumentExceptionWhenNotOnlyDigitsRowPassed() {
-        assertThrows(IllegalArgumentException.class, ()->validator.validate("0;1;L;3;D;5;X;7;D;"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("0;1;2;3;4;5;6;7;"));
     }
 
+    @Test
+    public void shouldRaiseInvalidArgumentExceptionWhenNotTooShortColsPassed() {
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("0;0;0;0;0;0;0;0;0;\n0;0;0;0;0;0;0;0;0;\n"));
+    }
+
+    @Test
+    public void shouldRaiseInvalidArgumentExceptionWhenNotOnlyDigitsRowPassed() {
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("0;1;L;3;D;5;X;7;D;"));
+    }
+
+    @Test
+    public void shouldPassWhenFullFilledWithDigitsBoardPassed() {
+        assertDoesNotThrow(() -> validator.validate("0;0;0;0;0;0;0;0;0\n" +
+                "0;0;0;0;0;0;0;0;0\n" +
+                "0;0;0;0;0;0;0;0;0\n" +
+                "0;0;0;0;0;0;0;0;0\n" +
+                "0;0;0;0;0;0;0;0;0\n" +
+                "0;0;0;0;0;0;0;0;0\n" +
+                "0;0;0;0;0;0;0;0;0\n" +
+                "0;0;0;0;0;0;0;0;0\n" +
+                "0;0;0;0;0;0;0;0;0\n"));
+    }
 }
